b6da3b16e9d011ed617657ac2bb5b4ac87ca122e
anthonyjoyner <anthony.c.joyner@gmail.com>
2020-04-23 01:00:43 -0400

    Added functionality to show listing on the front end of the website.  The listings include the title, price, and images.

--- my_app/views.py
+++ my_app/views.py
@@ -6,6 +6,8 @@ from . import models
 
 # Base URL to capture the searches dynamically.
 BASE_CRAIGSLIST_URL = 'https://charlotte.craigslist.org/search/ata?query={}'
+# Base URL to capture the images of each search
+BASE_IMAGE_URL='https://images.craigslist.org/{}_300x300.jpg'
 # Create your views here.
 def home(request):
     return render(request,'base.html')
@@ -23,15 +25,34 @@ def new_search(request):
     soup = BeautifulSoup(data, features='html.parser')
 
     post_listings = soup.find_all('li',{'class':'result-row'})
-    post_title = post_listings[0].find(class_='result-title hdrlnk').text
-    post_url = post_listings[0].find('a').get('href')
-    post_price = post_listings[0].find(class_='result-price').text
-    #
-    print(post_title)
-    print(post_url)
-    print(post_price)
+    # Test to check if the soup object is working correctly.  Pull the first item in the object
+    # post_title = post_listings[0].find(class_='result-title hdrlnk').text
+    # post_url = post_listings[0].find('a').get('href')
+    # post_price = post_listings[0].find(class_='result-price').text
+
+    final_postings = []
+
+    for post in post_listings:
+        post_title = post.find(class_='result-title hdrlnk').text
+        post_url = post.find('a').get('href')
+        post_price = post.find(class_='result-price').text
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'N/A'
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            print(post_image_id)
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+            print(post_image_url)
+        else:
+            post_image_url = 'https://craigslist.org/images/peace.jpg'
+
+        final_postings.append((post_title,post_url,post_price,post_image_url))
 
     stuff_for_front_end = {
         'search': search,
+        'final_postings':final_postings,
     }
     return render(request, 'my_app/new_search.html',stuff_for_front_end)
